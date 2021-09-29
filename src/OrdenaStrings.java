e2059f6129a3183eed43e6602b05384608eac0ef
Vitor Albano <vitoralbano@gmail.com>
2021-09-29 08:25:40 -0300

    sort method from List

---
+++ src/OrdenaStrings.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class OrdenaStrings {
+    public static void main(String[] args) {
+        List<String> palavras = new ArrayList<>();
+        palavras.add("long life learning");
+        palavras.add("cursos online");
+        palavras.add("recursos java 8");
+
+        palavras.sort(new ComparadorPorTamanho());
+        System.out.println(palavras);
+    }
+}
+
+class ComparadorPorTamanho implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        return Integer.compare(o1.length(), o2.length());
+    }
+}
